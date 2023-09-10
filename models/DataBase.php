937a88843999346923d8365c544e51a8d876c93c
lolminder <lolminder3@ya.ru>
2023-09-10 20:19:56 +0300

    &&
    &&

--- models/DataBase.php
+++ models/DataBase.php
@@ -17,18 +17,18 @@ class DataBase extends ActiveRecord
 
 class BaseData1 extends ActiveRecord {
     public  function getPerson(){
-        return $this->hasMany(DataBase::class,['car_id'=>'person_id'])||$this->hasMany(DataBase::class,['work_id'=>'person_id']);
+        return $this->hasMany(DataBase::class,['car_id'=>'person_id'])&&$this->hasMany(DataBase::class,['work_id'=>'person_id']);
     }
 }
 
 class BaseData2 extends ActiveRecord{
     public  function getCar(){
-        return $this->hasMany(DataBase::class,['person_id'=>'car_id'])||$this->hasMany(DataBase::class,['person_id'=>'work_id']);
+        return $this->hasMany(DataBase::class,['person_id'=>'car_id'])&&$this->hasMany(DataBase::class,['person_id'=>'work_id']);
     }
 }
 
 class BaseData3 extends  ActiveRecord{
     public  function BD2(){
-        return $this->hasMany(DataBase::class,['work_id'=>'person_id'])||$this->hasMany(DataBase::class,['work_id'=>'car_id']);
+        return $this->hasMany(DataBase::class,['work_id'=>'person_id'])&&$this->hasMany(DataBase::class,['work_id'=>'car_id']);
     }
 }
